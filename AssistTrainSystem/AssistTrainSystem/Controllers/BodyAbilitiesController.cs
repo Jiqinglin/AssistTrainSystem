edc7d7bab3937fc58fffb18d4398058d253a0d55
SilentCC <17816853696@163.com>
2017-11-23 17:11:51 +0800

    AssistTrainSystem

--- AssistTrainSystem/AssistTrainSystem/Controllers/BodyAbilitiesController.cs
+++ AssistTrainSystem/AssistTrainSystem/Controllers/BodyAbilitiesController.cs
@@ -21,42 +21,43 @@ namespace AssistTrainSystem.Controllers
         // GET: BodyAbilities
         public async Task<IActionResult> Index()
         {
-            //从数据库提取用户
-            User user = await _context.User.SingleOrDefaultAsync(m => m.name == User.Identity.Name);
-            //从数据库中提取人员基本信息表,
             var bodyAbility = from bb in _context.BodyAbility select bb;
             bodyAbility = bodyAbility.Where(m => m.user_id == User.Identity.Name);
             List<BodyAbility> b = await bodyAbility.AsNoTracking().ToListAsync();
 
-            UserAbility list = new UserAbility(user, b);
+            var lastAbilit = await _context.BodyAbility.LastOrDefaultAsync(m => m.user_id == User.Identity.Name);
+            BodyList list = new BodyList();
+            list.alllist = b;
+            list.lastlist = lastAbilit;
 
-            
-         
-          
-            
-            foreach (var x in b)
+            foreach(var x in b)
             {
-                list.listtime.Add(x.create_time.Date.ToString("yyyy-MM-dd"));
-                List<string> item1 = new List<string>();
-                List<List<string>> temp = new List<List<string>>();
-                item1.Add(x.bfp.ToString());
-                item1.Add(x.bmi.ToString());
-                item1.Add((x.weight*1000000).ToString());
-                item1.Add(x.user_id.ToString());
-                item1.Add(x.create_time.Date.ToString("yyyy-MM-dd"));
-
-                temp.Add(item1);
-
-               
-
-                list.series.Add(temp);
-          
+                list.time.Add(x.create_time.ToString("MM-dd"));
+                list.bfp.Add(x.bfp.ToString());
+                list.bmi.Add(x.bmi.ToString());
             }
-          
-          
+            
             return View(list);
         }
-
+        public class BodyList
+        {
+            //用户的所有测试
+            public List<BodyAbility> alllist;
+            //用户的最后一次测试
+           public  BodyAbility lastlist;
+            //用户所有测试的时间
+            public List<string> time;
+            //用户所有测试的体脂率
+            public List<string> bfp;
+            public List<string> bmi;
+            public BodyList(){
+                alllist = new List<BodyAbility>();
+                lastlist = new BodyAbility();
+                time = new List<string>();
+                bfp = new List<string>();
+                bmi = new List<string>();
+            }
+        }
         // GET: BodyAbilities/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -86,15 +87,35 @@ namespace AssistTrainSystem.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,user_id,create_time,height,weight,age,waist,bmi,bfp")] BodyAbility bodyAbility)
+        public async Task<IActionResult> Create([Bind("user_id,height,weight,age,waist,")] BodyAbility bodyAbility)
         {
-            if (ModelState.IsValid)
-            {
+           bodyAbility.create_time = DateTime.Now;
+                //计算体脂率的公式
+                bodyAbility.bfp = (double)(bodyAbility.waist * 0.74 - (bodyAbility.weight * 0.082 + 44.74)) / bodyAbility.weight;
+                bodyAbility.bfp = bodyAbility.bfp * 100;
+                //计算BMI的公式
+                bodyAbility.bmi = (double) bodyAbility.weight / (bodyAbility.height * bodyAbility.height);
                 _context.Add(bodyAbility);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(bodyAbility);
+
+            result res = new result();
+             res.ans = "加油锻炼";
+            res.bfp = bodyAbility.bfp;
+            res.bmi = bodyAbility.bmi;
+            JsonResult result=Json(res);
+           
+
+                return result;
+          
+        }
+        public class result
+        {
+            //返回这个人的体脂率
+            public double bfp;
+            //返回这个人的bmi
+            public double bmi;
+            //返回建议
+            public string ans;
         }
 
         // GET: BodyAbilities/Edit/5
@@ -182,39 +203,7 @@ namespace AssistTrainSystem.Controllers
             return _context.BodyAbility.Any(e => e.ID == id);
         }
 
-        //人员自测首页，需要传送的model
-        public class UserAbility
-        {
-            //当前用户的信息
-            public User user;
-            //当前用户的历史人员基本信息
-            public List<BodyAbility>  bodyAbility;
-            //测试时间列表
-            public List<string> listtime;
-            //测试项目
-            public List<string> listitem = new List<string>{ "hh" };
-
-            public List<List<List<string>>> series;
-            public UserAbility(User user, List<BodyAbility> body)
-            {
-                this.user = user;
-                this.bodyAbility = body;
-                listtime = new List<string>();
-                series = new List<List<List<string>>>(100);
-                var i = 0;
-                foreach(var s in series)
-                {
-                    series[i] = new List<List<string>>(100);
-                    var j = 0;
-                    foreach(var m in series[i])
-                    {
-                        series[i][j] = new List<string>(100);
-                        j++;
-                    }
-                    i++;
-                }
-            }
-        }
+     
     }
    
 }
